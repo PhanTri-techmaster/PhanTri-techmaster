f9701fb0b731ba7b9dfdd4babdbcbddc0a805bc2
PhanTri-techmaster <65991362+PhanTri-techmaster@users.noreply.github.com>
2020-09-13 21:16:06 +0700

    update shopping cart

---
+++ html-template/js/cart-header.js
@@ -0,0 +1,11 @@
+function CartHeader(){
+    return(  
+    <header className="container">
+    <h1>Shopping Cart</h1>
+    <ul className="breadcrumb">
+      <li>Home</li>
+      <li>Shopping Cart</li>
+    </ul>
+    <span className="count">3 items in the bag</span>
+  </header>)
+  }
